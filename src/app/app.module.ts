2eed4876e0bc3baa5425cd2db9486464bbca2084
stefannytoror <stefannyrami@gmail.com>
2019-06-22 09:57:48 -0500

    DojoAngular

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EstudianteComponent } from './estudiante/estudiante.component';
+import { ClimaComponent } from './clima/clima.component';
+import {HttpClientModule} from "@angular/common/http";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EstudianteComponent,
+    ClimaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
